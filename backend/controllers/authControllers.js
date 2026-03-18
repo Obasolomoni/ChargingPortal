101cf40e42aa73d1b1e1fbaa4c8c42329c6f1b13
Kolade Pelumi <koladeanuoluwapelumikingsolomo@gmail.com>
2026-03-18 11:51:46 +0100

    Added userName endpoint

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -50,9 +50,9 @@ export const loginUser = async (req, res) => {
 
 export const getUserName = async (req, res) => {
   try {
-    const userName = await Users.findOne().select("userName")
-    res.json(userName);
-  } catch(error) {
-    res.status(500).json({message: err.message});
+    const user = await Users.findOne().select("username");
+    res.json({ username: user.username });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-} 
+};
