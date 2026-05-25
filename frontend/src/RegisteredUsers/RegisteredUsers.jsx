583ec4218d7664a4660c98f7484a70b32a7a8f61
Kolade Pelumi <koladeanuoluwapelumikingsolomo@gmail.com>
2026-05-25 16:15:04 +0100

    Updated UI

--- frontend/src/RegisteredUsers/RegisteredUsers.jsx
+++ frontend/src/RegisteredUsers/RegisteredUsers.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../Navbar/Navbar';
+import Loader from "../Loader/Loader";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import './RegisteredUsers.css';
